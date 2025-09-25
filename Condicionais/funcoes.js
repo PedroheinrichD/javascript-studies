01f6777eb5eb85ef9cd964b7877d59b636e371e3
Pedro Heinrich <pedroheinrich91@gmail.com>
2025-09-25 07:21:08 -0300

    Adicionada Exercicio (Calcule o preço do imóvel)

--- Condicionais/funcoes.js
+++ Condicionais/funcoes.js
@@ -60,7 +60,6 @@ console.log(result);
 
 /*Calcule a porcentagem entre 2 números.
 Exemplo: 25% de 40 é 10
-Fórmula da porcentagem: (y / x) * 100
 */
 
 
@@ -68,14 +67,57 @@ let porcentagem = 30
 let valor = 200
 
 function calcPct(porcentagem, valor) {
-   let resultC = (valor * porcentagem) / 100
-   return resultC.toFixed(2) + "%"
+    let resultC = (valor * porcentagem) / 100
+    return resultC.toFixed(2) + "%"
 }
 
 
 
-let calcResult = calcPct(30,200)
+let calcResult = calcPct(30, 200)
 console.log(`${porcentagem}% de ${valor} é : ${calcResult} `);
+
+
+
+/*
+Calcule o preço do imóvel
+- m2 = 3.000
+- Se tiver 1 quarto, o m2 é 1x
+- Se tiver 2 quartos, o m2 é 1.2x
+- Se tiver 3 quartos, o m2 é 1.5x
+*/
+
+//Valor base do m2 = R$ 3.000 (custo de CADA m²)
+let m2 = 3000
+
+function precoImovel(metragem, quartos) {
+    let multplicador
+
+    switch (quartos) {
+        case 1:
+            multplicador = 1.0
+            break;
+        case 2:
+            multplicador = 1.2
+            break;
+        case 3:
+            multplicador = 1.5
+            break
+
+        default:
+            console.log(`Não há uma regra definida para ${quartos} quartos.`);
+            break;
+            return
+    }
+
+
+    let precoFinalImovel = (m2 * metragem ) * multplicador
+
+    console.log(`Imóvel de ${metragem}m² com ${quartos} quartos: R$ ${precoFinalImovel.toFixed(2)}`);
+}
+
+precoImovel(122,2) // exibe --> Imóvel de 122m² com 2 quartos: R$ 439200.00
+
+
 
 
 
