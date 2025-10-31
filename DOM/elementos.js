44d4653ba19e77348390e7e1a547a6c0534b113b
Pedro Heinrich <pedroheinrich91@gmail.com>
2025-10-31 07:22:31 -0300

    adicionar manipulação de elementos e evento de clique no DOM
    Implementa função mudarLista() para alterar e adicionar itens em uma lista <ul>
    - Utiliza children[], append(), createElement() e appendChild() para manipular o DOM
    - Adiciona evento de clique no botão para executar a função de modificação da lista

--- DOM/elementos.js
+++ DOM/elementos.js
@@ -1,8 +1,26 @@
-//Eventos de clique
+//Eventos de clique e Manipulação de elementos 
+let ul = document.querySelector("#teste1 ul") // guardando o elemento HTML em uma VAR
+let button = document.querySelector(".button")  // guardando o elemento HTML em uma VAR
 
-function alertClick(){
-    alert('Você clicou no botão ') 
+
+function mudarLista() {
+    ul.children[2].innerHTML = '<li>Mudouuu</li>' // alterando o item 2 do UL
+    // A propriedade children é usada para acessar o elementos [Filho] --> exibe em um array 
+    // ul -> pai , li -> filho
+
+    // append funciona tanto para texto, tanto para outros tipos de elementos
+    ul.children[0].append(` adicionou sem modificar`)// o append é quando eu quero adiconar um conteudo junto ou dentro de um conteudo sem modifica-lo/(remove-lo) 
+
+
+    // adicionando um novo item a minha lista sem alterá-la
+    let newLi = document.createElement("li") // criando o HTML </li>        
+    newLi.innerText = 'Novo li' // guardando valor no novo </li>
+    ul.appendChild(newLi) // adicionando na lista </ul>
+
+
+
+    // o prepend() funciona igual , só que ele adiciona no primeiro item e não no ultimo 
 }
+// innerHTML -> exibe um texto com o conteúdo dentro, ou fazer alterações
+button.addEventListener("click", mudarLista) // evento de click que chama a função (mudarLista) quando for acionado 
 
-let button = document.querySelector(".button")
-button.addEventListener("click",alertClick) // evento de click que chama a função (alertClick) quando for acionado 
