4b16a434cfed8cc66d1edc04a1b0faf448178ffb
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-03-06 09:06:25 -0300

    adiciona verificação de campo vazio do input e style novo para a visualização

--- LISTA-DE-TAREFAS/script.js
+++ LISTA-DE-TAREFAS/script.js
@@ -3,7 +3,9 @@ let ul = document.querySelector('ul') // selecionando elemento ul(lista) do HTML
 input.addEventListener('keyup', enter) // adicioando evento de tecla
 
 function enter(event) { // funcão para adicionar lista
-    if (event.code === 'Enter') { // se a tecla enter for pressionada
+
+    // .trim() ->  remove espaços no começo e no final do texto.
+    if (event.code === 'Enter' && input.value.trim() !== '') { // se a tecla enter for pressionada
         let newLi = document.createElement('li') // cria elemento <li></li>
         let checkbox = document.createElement('input') // cria elemento de input
         checkbox.type = 'checkbox' // muda o type para checkbox
@@ -14,6 +16,17 @@ function enter(event) { // funcão para adicionar lista
         newLi.appendChild(checkbox) // adiciona checkbox no <li>
         newLi.appendChild(span); // adiciona o span no <li>  
         ul.appendChild(newLi) // newLi é add na lista
+
+        checkbox.addEventListener('change', function () { // função para riscar checkbox marcado
+            if (this.checked) {
+                span.style.textDecoration = 'line-through'
+                span.style.color = '#888'
+            } else {
+                span.style.textDecoration = 'none'
+                span.style.color = '#000'
+            }
+        })
+
         input.value = '' // limpa o campo do input
     }
 }
