69115717d1c4f9b2f5a4eda3069cc6058b25ebe0
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-03-10 07:32:17 -0300

    Adiciona exemplos de get e set usando a idade e o primeiro nome e ultimo nome

--- JS-AVANÇADO/script.js
+++ JS-AVANÇADO/script.js
@@ -1,60 +1,45 @@
 
-
-/*
-     o que é OO? 
-     É uma abordagem de programação onde os códigos são agrupados em objetos 
-     que se comunicam entre si.
-
-
-     OOP -> Object Oriented Programming 
-
-     this -> se refere ao proprio objeto 
-*/
-
-
 // criando um "molde" -> class
-
-
 class Person { // class é um conjunto de atributos e métodos que definem um objeto
 
     steps = 0; // criando passos 
+    _age = 0; // criando idade
 
-    constructor(name) { // construindo
-        this.name = name
+    constructor(FirstName, LastName) { // construindo
+        this.FirstName = FirstName
+        this.LastName = LastName
     }
 
-    //Ações
-    takeAStep() {
-        this.steps++
+    // USANDO GETTER -> SEMPRE RETORNA -> Um getter é usado para acessar valores
+    get fullName() { // nome completo ex:
+        return `${this.FirstName + this.LastName}`
     }
 
-    setAge(newAge) {
-        if (typeof newAge == 'number') {
-            this.age = newAge
-        }else{
-            console.log(`Número inválido`);
-            
+    get age(){  
+        return this._age // _age é uma variavel interna-> acessando o valor 
+    }
+
+
+    // USANDO O SETTER -> Setter é usado para definir ou modificar valores.
+    set age(x) { // x = 20
+        if (typeof x == 'number') {
+            this._age = x // this._age = 20 --> o 20 vai ser armazenado em (_age)
         }
     }
 
 }
 
 // criando a instância
-const p1 = new Person("Pedro");
-p1.setAge(20); // adicionando a idade pela função 
+const p1 = new Person("Pedro", " DIAS");
+p1.age = 20 // executando o setter
 
-const p2 = new Person("joão");
-p2.setAge(22); // adicionando a idade pela função 
-
-
-const p3 = new Person("Alberto")
-p3.takeAStep() // executando ação de passos
-p3.takeAStep() // executando ação de passos
+const p3 = new Person("Alberto", " Miranda")
+p3.age = 55 // executando o setter
 
 
 
 
-console.log(`${p1.name} tem ${p1.age} anos de idade`);
-console.log(`${p2.name} tem ${p2.age} anos de idade`);
-console.log(`${p3.name} tem ${p3.age} anos de idade e deu ${p3.steps} PASSOS`);
+//chamando a função fullName  
+console.log(`${p1.fullName} tem ${p1.age} anos de idade`);
+console.log(`${p3.fullName} tem ${p3.age} anos de idade e deu ${p3.steps} PASSOS`);
 
