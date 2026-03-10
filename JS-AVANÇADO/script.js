d6f522e18edfb73db616af0eec66a580563e3e57
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-03-10 06:33:05 -0300

    Adiciona exemplos de ações em classes, como verificação do typeof (em JS-AVANÇADO)

--- JS-AVANÇADO/script.js
+++ JS-AVANÇADO/script.js
@@ -16,18 +16,45 @@
 
 
 class Person { // class é um conjunto de atributos e métodos que definem um objeto
-    constructor(name, age) { // construindo
-        this.name = name,
-            this.age = age
+
+    steps = 0; // criando passos 
+
+    constructor(name) { // construindo
+        this.name = name
     }
+
+    //Ações
+    takeAStep() {
+        this.steps++
+    }
+
+    setAge(newAge) {
+        if (typeof newAge == 'number') {
+            this.age = newAge
+        }else{
+            console.log(`Número inválido`);
+            
+        }
+    }
+
 }
 
 // criando a instância
-const p1 = new Person("Pedro", 20);
-const p2 = new Person("joão", 22);
-const p3 = new Person("José", 19);
+const p1 = new Person("Pedro");
+p1.setAge(20); // adicionando a idade pela função 
+
+const p2 = new Person("joão");
+p2.setAge(22); // adicionando a idade pela função 
+
+
+const p3 = new Person("Alberto")
+p3.takeAStep() // executando ação de passos
+p3.takeAStep() // executando ação de passos
+
+
+
 
 console.log(`${p1.name} tem ${p1.age} anos de idade`);
 console.log(`${p2.name} tem ${p2.age} anos de idade`);
-console.log(`${p3.name} tem ${p3.age} anos de idade`);
+console.log(`${p3.name} tem ${p3.age} anos de idade e deu ${p3.steps} PASSOS`);
 
