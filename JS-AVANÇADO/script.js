4742a0e3370595e342e4b79d03ca1c45c6af5176
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-03-10 14:56:52 -0300

    Adiciona explicação sobre como funciona o GET e SETTER

--- JS-AVANÇADO/script.js
+++ JS-AVANÇADO/script.js
@@ -1,3 +1,11 @@
+/*
+    GETTER /  roda o getter sempre que o valor é pedido ex: p1.age
+     alguém pede o valor -> getter executa -> getter retorna algo
+
+     SETTER / roda o setter sempre que o valor é atribuido ex: p1.age = (valor)
+     alguém define o valor -> setter executa -> setter decide o que fazer com esse valor 
+*/
+
 
 // criando um "molde" -> class
 class Person { // class é um conjunto de atributos e métodos que definem um objeto
@@ -10,13 +18,13 @@ class Person { // class é um conjunto de atributos e métodos que definem um ob
         this.LastName = LastName
     }
 
-    // USANDO GETTER -> SEMPRE RETORNA -> Um getter é usado para acessar valores
+    // USANDO GETTER 
     get fullName() { // nome completo ex:
         return `${this.FirstName + this.LastName}`
     }
 
-    get age(){  
-        return this._age // _age é uma variavel interna-> acessando o valor 
+    get age(){ //  
+        return this._age // _age é uma variavel interna -> acessando o valor 
     }
 
 
@@ -31,15 +39,16 @@ class Person { // class é um conjunto de atributos e métodos que definem um ob
 
 // criando a instância
 const p1 = new Person("Pedro", " DIAS");
-p1.age = 20 // executando o setter
+p1.age = 20 // executando o setter. Toda vez que temos um valor sendo atribuído SEMPRE CAIRÁ NO SETTER
 
 const p3 = new Person("Alberto", " Miranda")
-p3.age = 55 // executando o setter
+p3.age = 55 // executando o setter. Toda vez que temos um valor sendo atribuído SEMPRE CAIRÁ NO SETTER
 
 
 
 
-//chamando a função fullName  
+//chamando a função fullName   
+// p1.age -> VAI CAIR NO GET , ou seja , o getter entrega vaçor para quem pediu o valor -> nesse caso o console.log()
 console.log(`${p1.fullName} tem ${p1.age} anos de idade`);
 console.log(`${p3.fullName} tem ${p3.age} anos de idade e deu ${p3.steps} PASSOS`);
 
