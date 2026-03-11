845f2ded4bc944c7ca14e10263274c742d855d79
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-03-11 06:13:17 -0300

    Criação de arquivos para exercicio prático
    Adiciona arquivo contaBancaria.HTML
    
    Adiciona arquivo contaBancaria.JS
    
    Adiciona desafio

---
+++ Exercicios_Praticos/CONTA_BANCARIA/contaBancaria.js
@@ -0,0 +1,41 @@
+/*
+Desafio 2 — (Conta Bancária)
+Você precisa criar uma classe chamada ContaBancaria.
+
+A conta deve possuir um saldo interno que começa com valor 0.
+
+Esse saldo não pode ser acessado diretamente fora da classe.
+
+Agora crie duas coisas:
+
+1️⃣ Um mecanismo para depósito
+
+Regras do depósito:
+	•	só pode aceitar números
+	•	o valor precisa ser maior que 0
+	•	quando o valor for válido, ele deve ser somado ao saldo atual
+	•	se o valor for inválido, nada acontece
+
+Exemplo de comportamento esperado:
+
+saldo começa em 0
+
+se alguém depositar 100
+saldo passa a ser 100
+
+se depois depositar 50
+saldo passa a ser 150
+
+se tentar depositar -20
+o saldo permanece 150
+
+⸻
+
+2️⃣ Um mecanismo para consultar o saldo
+
+Você precisa de uma forma de ver o saldo atual.
+
+Essa ação:
+	•	não pode modificar o saldo
+	•	apenas retorna o valor atual
+*/
